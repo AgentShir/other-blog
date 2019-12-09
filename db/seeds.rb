b04c296ce71af43bc5e8377140c8ab54050b88df
Shireen Garcia <shireen@scoutapm.com>
2019-12-09 15:01:17 +0900

    Even more records

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-100.times do
+1000.times do
     a = Article.create(
         title: Faker::TvShows::DrWho.the_doctor,
         text: Faker::TvShows::Friends.quote
